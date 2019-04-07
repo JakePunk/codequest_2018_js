b2311a9bb1a1104c4bf1dbc039d952a092d282a9
Dan Lynn <gitkraken@danlynn.org>
2019-04-07 14:38:35 -0400

    Initial commit

---
+++ Prob01/Prob01.mjs
@@ -0,0 +1,36 @@
+/**
+ * Read a file one line at a time (kindof)
+ */
+import fs from 'fs'
+import readline from 'readline'
+
+(async () => {
+  const fileStream = fs.createReadStream('Prob01.in.txt');
+  const readlineInterface = readline.createInterface({
+    input: fileStream
+  });
+  let lineIdx = 0;
+  let testCaseCount = 0;
+
+  try {
+    for await (const line of readlineInterface) {
+      if (lineIdx === 0) {
+        testCaseCount = parseInt(line);
+        console.log(`testCaseCount: ${testCaseCount}`);
+      }
+      else {
+        if (lineIdx > testCaseCount) {
+          readlineInterface.close(); // terminate further processing of file
+        }
+        else {
+          console.log(`${lineIdx}: ${line}`);
+          // process 'line' from file
+        }
+      }
+      lineIdx += 1;
+    }
+  } catch(err) {
+    console.error(err);
+  }
+})();
+
