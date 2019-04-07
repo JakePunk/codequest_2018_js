920981de07bbf25b4c1cd80feeb9c5371a20c07f
Dan Lynn <gitkraken@danlynn.org>
2019-04-07 16:04:43 -0400

    Implemented logic to process each line for PASS/FAIL

--- Prob01/Prob01.mjs
+++ Prob01/Prob01.mjs
@@ -16,15 +16,21 @@ import readline from 'readline'
     for await (const line of readlineInterface) {
       if (lineIdx === 0) {
         testCaseCount = parseInt(line);
-        console.log(`testCaseCount: ${testCaseCount}`);
+        // console.log(`testCaseCount: ${testCaseCount}`);
       }
       else {
         if (lineIdx > testCaseCount) {
           readlineInterface.close(); // terminate further processing of file
         }
         else {
-          console.log(`${lineIdx}: ${line}`);
+          // console.log(`${lineIdx}: ${line}`);
           // process 'line' from file
+          if (parseInt(line) >= 70) {
+            console.log("PASS");
+          }
+          else {
+            console.log("FAIL");
+          }
         }
       }
       lineIdx += 1;
